6de61f01b34caff37dfc844b98bf0250e81ebddd
Tarek Sallam <tarek@sallam.ca>
2024-12-23 12:27:10 -0500

    built header and hero

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let logoSrc = './logoWhite.svg';
+</script>
+
+<header class="bg-black px-4 py-4 text-lg text-white">
+	<div class="mx-auto flex w-full justify-between px-8 py-3">
+		<div class="h-auto w-12">
+			<img src={logoSrc} alt="Logo" />
+		</div>
+
+		<nav class="flex justify-between space-x-10">
+			<a href="/">About</a>
+			<a href="/">Projects</a>
+		</nav>
+	</div>
+</header>
+
+<style>
+	header {
+		width: 100%;
+	}
+</style>
---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,29 @@
+<script>
+	const handleContact = () => {
+		alert('Contact me at example@example.com!');
+	};
+
+	const handleResume = () => {
+		window.open('', '_blank');
+	};
+</script>
+
+<div class="flex flex-grow items-center justify-center bg-black text-white">
+	<div class="text-center">
+		<h1 class="mb-4 text-6xl font-bold">Tarek Sallam</h1>
+		<div>
+			<button
+				class="mb-2 rounded-md bg-blue-500 px-6 py-2 text-white shadow-md hover:bg-blue-700"
+				on:click={handleContact}
+			>
+				Contact Me
+			</button>
+			<button
+				class="rounded-md bg-green-500 px-6 py-2 text-white shadow-md hover:bg-green-700"
+				on:click={handleResume}
+			>
+				Resume
+			</button>
+		</div>
+	</div>
+</div>
