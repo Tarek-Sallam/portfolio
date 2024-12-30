76d79c53d0dca1c823ad1f6a54cf11e63bfbdd03
Tarek Sallam <tarek@sallam.ca>
2024-12-29 22:58:40 -0500

    added bg for about

--- src/lib/components/AboutMe.svelte
+++ src/lib/components/AboutMe.svelte
@@ -10,19 +10,23 @@
 			<h2 class="font-mono text-3xl font-bold text-black antialiased dark:text-white sm:text-5xl">
 				About Me
 			</h2>
-			<p
-				class="font-mono text-base tracking-tighter text-gray-600 antialiased dark:text-gray-300 sm:text-xl sm:tracking-normal"
+			<div
+				class="rounded-lg bg-gray-100 bg-opacity-75 px-4 py-4 shadow-md dark:bg-black dark:bg-opacity-75 sm:px-6 sm:py-6 lg:px-8 lg:py-8 xl:px-10 xl:py-10"
 			>
-				Hello! I'm Tarek, a Computer Science student at Carleton University! I am pursuing a minor
-				in mathematics and a concentration in AI & ML. I am fascinated by machine learning,
-				specifically deep learning and its applications and also enjoy learning about various
-				mathematical and statistical models. Feel free to explore what I'm currently working on (or
-				have previously worked on) in the projects section, or on my GitHub!
-				<br />
-				<br />
-				Outside of learning I enjoy spending time lifting weights and rock climbing, or relaxing at home
-				while watching a movie or playing my favourite video game!
-			</p>
+				<p
+					class="font-mono text-base tracking-tighter text-gray-600 antialiased dark:text-gray-300 sm:text-xl sm:tracking-normal"
+				>
+					Hello! I'm Tarek, a Computer Science student at Carleton University! I am pursuing a minor
+					in mathematics and a concentration in AI & ML. I am fascinated by machine learning,
+					specifically deep learning and its applications and also enjoy learning about various
+					mathematical and statistical models. Feel free to explore what I'm currently working on
+					(or have previously worked on) in the projects section, or on my GitHub!
+					<br />
+					<br />
+					Outside of learning I enjoy spending time lifting weights and rock climbing, or relaxing at
+					home while watching a movie or playing my favourite video game!
+				</p>
+			</div>
 		</div>
 	</div>
 </section>
