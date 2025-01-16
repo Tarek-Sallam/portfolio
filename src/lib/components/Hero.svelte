56f4e32a677b7c7f828fe7b8b4dcafa2549cc5d2
Tarek-Sallam <tarek.i.sallam@gmail.com>
2025-01-16 02:12:32 -0500

    fixed resume

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -39,7 +39,7 @@
 				<img src={linkedinIcon} alt="LinkedIn" class="h-10 w-10 lg:h-14 lg:w-14" />
 			</a>
 			<a
-				href="/Tarek_Sallam_Resume_NoPhone.pdf"
+				href="/Tarek_Sallam_Resume_ML_NoPhone.pdf"
 				target="_blank"
 				class="text-black transition-transform duration-300 hover:scale-110 dark:text-white"
 			>
